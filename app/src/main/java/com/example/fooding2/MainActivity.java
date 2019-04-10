c5485b2e6e5f1c804ebfc4b3dd4754f6c901cce8
LasagneArrosto <lasagne.arrosto@gmail.com>
2019-04-10 13:35:25 +0200

    First Commit

---
+++ app/src/main/java/com/example/fooding2/MainActivity.java
@@ -0,0 +1,31 @@
+package com.example.fooding2;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button profile_btn;
+    private Button menu_btn;
+    private Button orders_btn;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        profile_btn=findViewById(R.id.profile_btn);
+        menu_btn=findViewById(R.id.menu_btn);
+        orders_btn=findViewById(R.id.order_btn);
+
+        profile_btn.setOnClickListener(e -> {
+            Intent i=new Intent(getApplicationContext(), ProfileActivity.class);
+            startActivity(i);
+
+        });
+    }
+}
